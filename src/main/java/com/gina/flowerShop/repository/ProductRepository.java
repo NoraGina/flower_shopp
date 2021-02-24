23b45aa128c9e110f571f2ce8cbd3d29d7127fdb
Gina <gina.agapescu@gmail.com>
2021-02-24 10:33:28 +0200

    Adding in ProductRepository query for select distinct origin

--- src/main/java/com/gina/flowerShop/repository/ProductRepository.java
+++ src/main/java/com/gina/flowerShop/repository/ProductRepository.java
@@ -2,8 +2,13 @@ package com.gina.flowerShop.repository;
 
 import com.gina.flowerShop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT DISTINCT p.origin FROM Product p")
+    List<String> findDistinctOrigin();
 }
