5955bd998271e959cab4821439c507b124cabf8d
Gina <gina.agapescu@gmail.com>
2021-02-24 10:36:23 +0200

    Adding in ProductRepository queries for filter products

--- src/main/java/com/gina/flowerShop/repository/ProductRepository.java
+++ src/main/java/com/gina/flowerShop/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.gina.flowerShop.repository;
 import com.gina.flowerShop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,13 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT DISTINCT p.origin FROM Product p")
     List<String> findDistinctOrigin();
+    
+    @Query("select p from Product p  INNER JOIN p.categories c where c.idCategory=:idCategory")
+    List<Product> findAllByCategoryId(@Param("idCategory")Long idCategory);
+
+    @Query("Select p from Product p where p.stock=:stock")
+    List<Product>findAllByStockGreaterThan(@Param("stock") int stock);
+
+    @Query("select p from Product p  INNER JOIN p.categories c where c.idCategory=:idCategory and p.stock=:stock")
+    List<Product> findAllByCategoryIdAndStockGreaterThan(@Param("idCategory")Long idCategory, @Param("stock")int stock);
 }
