1daf81acd92f48f549f09428adf520489ebc7d3d
Gina <gina.agapescu@gmail.com>
2021-02-24 09:49:21 +0200

    Create ProductServiceImpl and implement ProductService methods

---
+++ src/main/java/com/gina/flowerShop/service/ProductServiceImpl.java
@@ -0,0 +1,81 @@
+package com.gina.flowerShop.service;
+
+import com.gina.flowerShop.model.Product;
+import com.gina.flowerShop.repository.ProductRepository;
+import com.gina.flowerShop.web.dto.ProductDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class ProductServiceImpl implements ProductService{
+    @Autowired
+    ProductRepository productRepository;
+
+    private ProductDto toDTO(Product product) {
+        final ProductDto productDto = new ProductDto();
+        productDto.setIdProduct(product.getIdProduct());
+        productDto.setProductName(product.getProductName());
+        productDto.setImage(product.getImage());
+        productDto.setDescription(product.getDescription());
+        productDto.setPrice(product.getPrice());
+        productDto.setOrigin(product.getOrigin());
+        productDto.setCategories(product.getCategories());
+        productDto.setStock(product.getStock());
+
+        return productDto;
+    }
+
+    private Product toEntity(ProductDto productDto) {
+        final Product product = new Product();
+        if (productDto.getIdProduct() != null) {
+            product.setIdProduct(productDto.getIdProduct());
+        }
+        product.setIdProduct(productDto.getIdProduct());
+        product.setProductName(productDto.getProductName());
+        product.setImage(productDto.getImage());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        product.setOrigin(productDto.getOrigin());
+        product.setCategories(productDto.getCategories());
+        product.setStock(productDto.getStock());
+
+        return product;
+    }
+
+    @Override
+    public void save(ProductDto productDto) {
+        final Product product = toEntity(productDto);
+
+        productRepository.save(product);
+    }
+
+    @Override
+    public void delete(Long idProduct) {
+        productRepository.deleteById(idProduct);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ProductDto> findAll() {
+        List<Product> productModelList = productRepository.findAll();
+        List<ProductDto> productDtoList = new ArrayList<>();
+        for (Product product : productModelList) {
+            ProductDto productDto = toDTO(product);
+            productDtoList.add(productDto);
+        }
+        return productDtoList;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<ProductDto> findOne(Long idProduct) {
+        return productRepository.findById(idProduct).map(this::toDTO);
+    }
+
+}
